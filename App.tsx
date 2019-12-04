29fcf2932d85adb9ddaca61e1b46f813b1a02ff5
AkhilMurali336 <58228108+AkhilMurali336@users.noreply.github.com>
2019-12-04 11:49:02 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello world</Text>
+      <Text >first attempt</Text>
+      <Text>success</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
